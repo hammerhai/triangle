d593322658a0242790ee2c9d473fa8cff4c68ab2
Nathan <43486313+hammerhai@users.noreply.github.com>
2022-03-02 01:11:26 -0500

    📝 Update documentation

--- example/directories/bin/directories.dart
+++ example/directories/bin/directories.dart
@@ -4,12 +4,14 @@ void main() {
   TriangleProject project = TriangleProject("directories-example");
 
   String? _configuration = project.findConfigurationDirectory()?.path ?? "";
+  String? _home = Triangle.home?.path ?? "";
   String? _localCache = project.findLocalCacheDirectory()?.path ?? "";
   String? _localData = project.findLocalDataDirectory()?.path ?? "";
   String? _preference = project.findPreferenceDirectory()?.path ?? "";
   String? _roamingData = project.findRoamingDataDirectory()?.path ?? "";
 
   print("Configuration: $_configuration");
+  print("Home: $_home");
   print("Local cache: $_localCache");
   print("Local data: $_localData");
   print("Preference: $_preference");
